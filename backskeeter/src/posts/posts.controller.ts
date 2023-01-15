1596406b953e703501d85b4df96b7b170554e42a
adriel <adrielarochaoronoz@gmail.com>
2023-01-15 17:32:14 +0000

    🦎 adding endpoint in post controller

---
+++ backskeeter/src/posts/posts.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Posti } from '@prisma/client';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+  @Get()
+  async getPost(
+    @Param('pages') pages: number,
+    @Param('limit') limit: number,
+  ): Promise<Posti> {
+    return this.postService.getPost(pages, limit);
+  }
+
+  @Get('user')
+  async getPostByUser(
+    @Param('userId') userId: string,
+    @Param('pages') pages: number,
+    @Param('limit') limit: number,
+  ): Promise<Posti> {
+    return this.postService.getPostByUser(userId, pages, limit);
+  }
+
+  @Post()
+  async createPost(@Body() newPost: Posti): Promise<Posti> {
+    return this.postService.create(newPost);
+  } 
+
+  @Put('id')
+  async updatePost(@Param('id') id: string, @Body() updatedPost: Posti): Promise<Posti> {
+    return this.postService.update(id, updatedPost);
+  }
+
+  @Delete('id')
+  async deletePost(@Param('id') id: string): Promise<Posti> {
+    return this.postService.delete(id);
+  }
+}
