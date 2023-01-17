507e9e3288eda0f44d404c1759963a11d6cbece5
adriel <adrielarochaoronoz@gmail.com>
2023-01-17 18:39:04 +0000

    🦎 adding basic endpoints for user

--- backskeeter/src/users/users.controller.ts
+++ backskeeter/src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+  @Get()
+  async getUser(
+    @Param('pages') pages: number,
+    @Param('limit') limit: number,
+  ): Promise<User[]> {
+    return this.userService.getUser(pages, limit);
+  }
+
+  @Post('/create')
+  async saveUser(@Body() user: User): Promise<User> {
+    return this.userService.saveUser(user);
+  }
+
+  @Delete('/delete')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return this.userService.deleteUser(id);
+  }
+
+  @Put('/update')
+  async updateUser(@Body() user: User): Promise<User> {
+    return this.userService.saveUser(user);
+  }
+}
--- backskeeter/src/users/users.service.ts
+++ backskeeter/src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUser(pages: number, limit: number): Promise<User[]> {
+    return this.prisma.user.findMany({
+      take: pages,
+    });
+  }
+
+  async getUserById(id: string): Promise<User> {
+    return this.prisma.user.findFirst({ where: { id } });
+  }
+
+  async deleteUser(id: string): Promise<User> {
+    return this.prisma.user.delete({ where: { id } });
+  }
+
+  async saveUser(user: User): Promise<User> {
+    return this.prisma.user.upsert({
+      create: {
+        isBanned: '',
+        isDeleted: '',
+        userEmail: user.userEmail,
+        strikes: 'none',
+      },
+      update: {
+        isBanned: user.isBanned,
+        isDeleted: user.isDeleted,
+        strikes: user.strikes,
+        userEmail: user.userEmail,
+      },
+      where: { id: user?.id },
+    });
+  }
+}
